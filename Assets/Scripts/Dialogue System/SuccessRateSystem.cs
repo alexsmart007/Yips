51e97c14082ce3a31afb7d17ae5425348195ddc4
Alex Contreras <57280113+alexsmart007@users.noreply.github.com>
2023-04-09 19:52:35 -0400

    Added in end screen

--- Assets/Scripts/Dialogue System/SuccessRateSystem.cs
+++ Assets/Scripts/Dialogue System/SuccessRateSystem.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Diagnostics;
 using UnityEngine;
+using TMPro;
 
 public class SuccessRateSystem : MonoBehaviour
 {
@@ -10,10 +11,36 @@ public class SuccessRateSystem : MonoBehaviour
     private int success;
     private int fail;
 
+    [SerializeField] private GameObject successText;
+    [SerializeField] private GameObject failText;
+    [SerializeField] private GameObject successRateText;
+    [SerializeField] private GameObject EndText;
+
+    [SerializeField] private int totalNumberOfSuspects;
+    [SerializeField] private float timeToFade;
+    [SerializeField] private float timeBeforeQuit;
+
+    private TextMeshProUGUI SuccessText;
+    private TextMeshProUGUI FailText;
+    private TextMeshProUGUI SuccessRateText;
+
     private List<string> unlocks;
+
+    void Start()
+    {
+        SuccessText = successText.GetComponent<TextMeshProUGUI>();
+        SuccessText.text = "Number Of Successful Deductions: 0";
+        FailText = failText.GetComponent<TextMeshProUGUI>();
+        FailText.text = "Number Of Failed Deductions: 0";
+        SuccessRateText = successRateText.GetComponent<TextMeshProUGUI>();
+        SuccessRateText.text = "Ultimate Success Rate: 0%";
+    }
+
 
     public float CalculateSuccessRate()
     {
+        int numOfSuccess = 0;
+        int numOfFail = 0;
         unlocks = DialogueManager.Instance.DialogueUnlocks;
         foreach (string unlock in unlocks) 
         {
@@ -21,26 +48,45 @@ public class SuccessRateSystem : MonoBehaviour
             {
                 if (unlock.Split('_')[1].Equals("success"))
                 {
-                    success++;
+                    numOfSuccess++;
                 }
                 else if(unlock.Split('_')[1].Equals("fail"))
                 {
-                    fail++;
+                    numOfFail++;
                 }
             }
         }
-        if(fail+success != 0) successRate = success / (float)(fail + success);
-        print("Success " + success);
-        print("Fail " + fail);
-        success = 0;
-        fail = 0;
+        if(numOfFail + numOfSuccess != 0) successRate = numOfSuccess / (float)(numOfFail + numOfSuccess);
+
+        success = numOfSuccess;
+        fail = numOfFail;
         return successRate;
+    }
+
+    private IEnumerator TimeBeforeQuit()
+    {
+        yield return new WaitForSecondsRealtime(timeBeforeQuit);
+        Application.Quit();
     }
 
 
     void Update()
     {
-        print("Success Rate: " + CalculateSuccessRate());
+        CalculateSuccessRate();
+        if (success + fail >= totalNumberOfSuspects)
+        {
+            StartCoroutine(FadeToBlackSystem.TryCueFadeInToBlack(timeToFade));
+            successText.SetActive(true);
+            failText.SetActive(true);
+            successRateText.SetActive(true);
+            EndText.SetActive(true);
+            StartCoroutine(TimeBeforeQuit());
+        }
+        SuccessText.text = "Number Of Successful Deductions:                " + success;
+        FailText.text = "Number Of Failed Deductions:                    " + fail;
+        SuccessRateText.text = "Ultimate Success Rate:                   " + successRate + "%";
+
     }
+
 
 }
